fff831ebc98424f23f5133ffed757580f238ee42
Nikita0728 <kunwornikita123@gmail.com>
2024-10-14 18:31:14 +0545

    feat: Logo added

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,21 +1,25 @@
-import React from 'react'
-import Link from 'next/link'
-import Image from 'next/image'
-import { useState, useEffect } from 'react'
-import {signIn, signOut, useSession, getProviders } from 'next-auth/react'
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import Logo from "../assets/Logo.png";
+import { useState, useEffect } from "react";
+import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 const Navbar = () => {
   return (
     <div>
-        nav
-        <nav className=''>
-            <Link href='/'>
-            {/* <Image/> */}
-            </Link>
-
-        </nav>
-
+      <nav className="flex flex-row gap-8 ">
+        <Link href="/">
+          <Image
+            className="w-[10rem] h-[5rem] flex  items-start pt-0"
+            src={Logo}
+            alt="Logo"
+          />
+        </Link>
+        <Link href="/About">Home</Link> <Link href="/Home">About</Link>
+        <Link href="/Contact"> Contact Us</Link>
+      </nav>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
